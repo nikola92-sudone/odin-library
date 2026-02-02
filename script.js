02bc0bd7dce2578dfaa238548b34e456318511ca
Nikola Milosevic <240391250+nikola92-sudone@users.noreply.github.com>
2026-02-02 16:48:38 +1100

    Add loop logic to render book objects as HTML cards

--- script.js
+++ script.js
@@ -13,4 +13,38 @@ function addBookToLibrary(title, author, pages, readStatus) {
     myLibrary.push(newBook)
 }
 
-document.creat
+function displayBooks() {
+    const display = document.querySelector('#library-display');
+    display.innerHTML = "";
+
+    myLibrary.forEach((book) =>{ 
+        const card = document.createElement('div');
+        card.classList.add('book-card');
+
+        const titleElement = document.createElement('h2');
+        titleElement.textContent = book.title;
+
+        const authorElement = document.createElement('p');
+        authorElement.textContent = `By: ${book.author}`;
+
+        const pagesElement = document.createElement('p');
+        pagesElement.textContent = `${book.pages} pages`;
+
+        const readStatusElement = document.createElement('p');
+        readStatusElement.textContent = book.readStatus ? "Read" : "Not Read";
+
+        card.appendChild(titleElement);
+        card.appendChild(authorElement);
+        card.appendChild(pagesElement);
+        card.appendChild(readStatusElement);
+
+        display.appendChild(card);})
+}
+
+// 1. Add some test data
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibrary("1984", "George Orwell", 328, false);
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+
+// 2. Run the display function
+displayBooks();
